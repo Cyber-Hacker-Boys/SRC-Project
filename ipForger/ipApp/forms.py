7e901425a08ae88e4624c39844a05b18fe2bc659
Fabian Nunes <fabiannunes12@gmail.com>
2021-11-07 19:09:10 +0000

    trying to do alert

--- ipForger/ipApp/forms.py
+++ ipForger/ipApp/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Div
 
 
 class IPCreatorForm(forms.Form):
---
+++ ipForger/ipApp/static/ipApp/scripts/script.js
@@ -0,0 +1,7 @@
+document.getElementById('packet-form').onsubmit = function () {
+    Swal.fire(
+      'Good job!',
+      'You sent a succesfull packet!',
+      'success'
+    )
+};
