7ee3302977fcae7784c93652d146de1d9de76b7c
Fabian Nunes <fabiannunes12@gmail.com>
2021-11-19 18:46:12 +0000

    changed the list to a table and added jquery mechanics

--- ipForger/ipApp/static/ipApp/scripts/sockets.js
+++ ipForger/ipApp/static/ipApp/scripts/sockets.js
@@ -14,12 +14,21 @@ socket.onclose = function(e){
 socket.onmessage = function (event) {
     var data = JSON.parse(event.data);
     console.log(data);
-
+/*
     let line = document.createElement("li");
     line.classList.add("new");
     let node = document.createTextNode(data);
     line.appendChild(node);
 
     let list = document.getElementById("table");
-    list.insertBefore(line, list.firstChild)
+    list.insertBefore(line, list.firstChild);*/
+
+    let row = document.createElement("tr");
+    let line = document.createElement("td");
+    let node = document.createTextNode(data);
+    line.appendChild(node);
+    row.appendChild(line)
+
+    let list = document.getElementById("entrie");
+    list.insertBefore(row, list.firstChild);
 }
