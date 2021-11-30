c2508bdaee4bb1d4eb47a590580539beb56b16e7
Fabian Nunes <fabiannunes12@gmail.com>
2021-11-30 11:52:52 +0000

    added forms but bug in images

---
+++ ipForger/ipApp/static/ipApp/scripts/modal.js
@@ -0,0 +1,31 @@
+let explanationE = [
+    'The ethernet header is the first piece added to the data in the second layer.',
+    'The Mac Address of the destination.',
+    'The Mac Address of the source.',
+    'Defines the type of protocol inside the frame, for example IPv4 or IPv6.'
+];
+
+let imageUrlE = [
+    "{% static 'ipApp/images/Ethernet.png' %}",
+    'images/destEther.png',
+    'img/srcEther.png',
+    'img/typeEther.png'
+]
+
+function helpUser(title, text, url, packet) {
+    var myModalEl = document.getElementById('helpModal');
+    console.log(title)
+    myModalEl.addEventListener('shown.bs.modal', function (e) {
+        document.getElementById('helpModalLabel').innerHTML = title;
+        switch (packet) {
+            case 0:
+                document.getElementById('helpModalText').innerHTML = explanationE[text];
+                document.getElementById('helpModelImage').src = imageUrlE[url];
+                break;
+            default:
+                break;
+        }
+
+    })
+}
+
